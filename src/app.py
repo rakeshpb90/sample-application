29cfa0229c39fc387d4486026545672eb5836477
rakeshpb90 <pb.rakesh90@gmail.com>
2021-02-09 15:27:24 +0530

    Add a new route for student

--- src/app.py
+++ src/app.py
@@ -12,5 +12,10 @@ def hello_world():
 def hello_user(username):
     return 'I have been Updated my friend %s!\n' % username
 
+
+@app.route('/student/<name>')
+def hello_world():
+    return f'Hi {name}!\n'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')     # open for everyone
